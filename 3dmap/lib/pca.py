a420f1166d8ba34a3a31d3229744b9d4c7e74239
Ryan Challener <rchallen@umich.edu>
2020-08-14 16:23:44 -0400

    Clarified PCA function. Added code to create eigenmaps and plot them. Added outdir option

--- 3dmap/lib/pca.py
+++ 3dmap/lib/pca.py
@@ -2,8 +2,26 @@ import numpy as np
 
 def pca(arr):
     """
+    Runs principle component analysis on the input array.
+
+    Arguments
+    ---------
+    arr: 2D array
+        n x m array, where m is the size of the dataset (e.g., times
+        in an observation) and n is the number of vectors
+
+    Returns
+    -------
+    evalues: 1D array
+        array of eigenvalues of size n
+
+    evectors: 2D array
+        n x n array of sorted eigenvectors
+
+    score: 2D array
+        n x m array of data projected in the new space
     """
-    
+    arr = arr.T
     # Subtract the mean
     m = (arr - np.mean(arr.T, axis=1)).T
     # Compute eigenvalues
@@ -13,5 +31,5 @@ def pca(arr):
     evalues  = evalues[   idx]
     evectors = evectors[:,idx]
     # Calculate projection of the data in the new space
-    score = np.dot(evectors, m)
-    return evectors, score, evalues
+    proj = np.dot(evectors, m)
+    return evalues, evectors, proj
--- 3dmap/run.py
+++ 3dmap/run.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 
+import os
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,61 +12,104 @@ def main():
     # Eventually read configuration here. For now, will assign values
 
     # General settings
-    lmax = 2
+    lmax = 3
+    outdir = 'testout'
 
     # Star
-    u = (0.5, 0.25)
     ms    = 1.0 # Mass in solar masses
     rs    = 1.0 # Radius in solar radii
     prots = 1.0 # Rotational period in days
 
     # Planet
-    mp    =  0    # Mass in solar masses
-    rp    =  0.1  # Radius in solar radii
-    porb  =  1.0  # Orbital period in days
-    protp =  1.0  # Rotational period in days
-    Omega = 30    # Long. of asc. node in deg
-    ecc   =  0.3  # Eccentricity
-    w     = 30    # Long of periastron in deg
-    t0    =  0    # Time of transit in days
-    flux  =  0.01 # Flux relative to star
+    mp    =  0.001 # Mass in solar masses
+    rp    =  0.1   # Radius in solar radii
+    porb  =  1.0   # Orbital period in days
+    protp =  1.0   # Rotational period in days
+    Omega =  0.0   # Long. of asc. node in deg
+    ecc   =  0.0   # Eccentricity
+    inc   = 88.5   # Inclination
+    w     = 90     # Long of periastron in deg
+    t0    =  0     # Time of transit in days
 
     # Observation
-    t = np.linspace(-0.25, 0.75, 3000)
+    nt = 1000
+    t = np.linspace(0.4*porb, 0.6*porb, nt)
 
-    # Create star object
-    star = starry.Primary(starry.Map(ydeg=0, udeg=len(u), amp=1),
+    # Create star, planet, and system objects
+    star = starry.Primary(starry.Map(ydeg=0, udeg=0, amp=1),
                           m=ms, r=rs, prot=prots)
 
-    # Set limb-darkening
-    star.map[1] = u[0]
-    star.map[2] = u[1]
+    planet = starry.kepler.Secondary(starry.Map(ydeg=lmax),
+                                     m=mp, r=rp, porb=porb,
+                                     prot=protp, Omega=Omega,
+                                     ecc=ecc, w=w, t0=t0,
+                                     inc=inc)
 
-    # Create harmonic maps of the planet
-    harmonics = []
-    for i, l in enumerate(range(lmax + 1)):
-        for j, m in enumerate(range(-l, l + 1)):
-            harm = starry.kepler.Secondary(starry.Map(ydeg = lmax),
-                                           m=mp, r=rp, porb=porb,
-                                           prot=protp, Omega=Omega,
-                                           ecc=ecc, w=w, t0=t0)
+    system = starry.System(star, planet)
 
-            if l > 0:
-                harm.map[l, m] = flux
+    # Create harmonic maps of the planet, excluding Y00
+    # (lmax**2 maps, plus a negative version for all but Y00)
+    nharm = 2 * ((lmax + 1)**2 - 1)
+    lcs = np.zeros((nharm, nt))
+    ind = 0
+    for i, l in enumerate(range(1, lmax + 1)):
+        for j, m in enumerate(range(-l, l + 1)):           
+            planet.map[l, m] =  1.0
+            sflux, lcs[ind]   = [a.eval() for a in system.flux(t, total=False)]
+            planet.map[l, m] = -1.0
+            sflux, lcs[ind+1] = [a.eval() for a in system.flux(t, total=False)]
+            planet.map[l, m] = 0.0
+            ind += 2
+            
 
-            harmonics.append(harm)
+    # Run PCA to determine orthogonal light curves
+    evalues, evectors, proj = pca.pca(lcs)
 
-    # Create systems for each harmonic map
-    systems = []
-    for i in range(len(harmonics)):
-        systems.append(starry.System(star, harmonics[i]))
+    # Convert orthogonal light curves into a map
+    eigeny = np.zeros((nharm, (lmax + 1)**2))
+    eigeny[:,0] = 1.0 # Y00 = 1 for all maps
+    for j in range(nharm):
+        yi  = 1
+        shi = 0
+        for l in range(1, lmax + 1):
+            for m in range(-l, l + 1):
+                eigeny[j,yi] = evectors.T[j,shi] - evectors.T[j,shi+1]
+                yi  += 1
+                shi += 2
 
-    # Evaluate light curves
-    lcs = np.zeros((len(systems), len(t)))
-    for i, system in enumerate(systems):
-        lcs[i] = system.flux(t).eval()
+    if not os.path.isdir(outdir):
+        os.mkdir(outdir)
 
-    evectors, score, evalues = pca.pca(lcs.T)
+    for j in range(nharm):
+        planet.map[1:,:] = 0
+
+        yi = 1
+        for l in range(1, lmax + 1):
+            for m in range(-l, l + 1):
+                planet.map[l, m] = eigeny[j, yi]
+                yi += 1
+
+        fill = np.int(np.floor(np.log10(nharm))) + 1
+        fnum = str(j).zfill(fill)
+        
+        fig, ax = plt.subplots(1, figsize=(5,5))
+        ax.imshow(planet.map.render(theta=180).eval(),
+                  origin="lower", cmap="plasma")
+        ax.axis("off")
+        plt.savefig(os.path.join(outdir, 'emap-ecl-{}.png'.format(fnum)))
+        plt.close(fig)
+
+        fig, ax = plt.subplots(1, figsize=(6,3))
+        ax.imshow(planet.map.render(projection="rect").eval(),
+                  origin="lower", cmap="plasma")
+        plt.savefig(os.path.join(outdir, 'emap-rect-{}.png'.format(fnum)))
+        plt.close(fig)
+                
+
+
+if __name__ == "__main__":
+    main()
+        
 
     
 
