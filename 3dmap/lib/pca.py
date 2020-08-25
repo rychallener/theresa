Fixed a bug in eigencurve calculation.

3dmap/lib/pca.py
@@ -7,7 +7,7 @@ def pca(arr):
     Arguments
     ---------
     arr: 2D array
-        m x n array, where m is the size of the dataset (e.g., times
+        (m, n) array, where m is the size of the dataset (e.g., times
         in an observation) and n is the number of vectors
 
     Returns
@@ -16,10 +16,10 @@ def pca(arr):
         array of eigenvalues of size n
 
     evectors: 2D array
-        n x n array of sorted eigenvectors
+        (n, n) array of sorted eigenvectors
 
-    score: 2D array
-        m x n array of data projected in the new space
+    proj: 2D array
+        (m, n) array of data projected in the new space
 
     Notes
     -----
@@ -35,5 +35,5 @@ def pca(arr):
     evalues  = evalues[   idx]
     evectors = evectors[:,idx]
     # Calculate projection of the data in the new space
-    proj = np.dot(evectors, m)
+    proj = np.dot(evectors.T, m)
     return evalues, evectors, proj