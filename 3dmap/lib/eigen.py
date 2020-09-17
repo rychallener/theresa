Added an option to enforce flux positivity (currently only checks eigenmap minimum and maximum locations). Fixed bugs with temperature map calculation.

3dmap/lib/eigen.py
@@ -76,7 +76,8 @@ def mkcurves(system, t, lmax):
 
     return eigeny, evalues, evectors, proj, lcs
 
-def mkmaps(planet, eigeny, params, npar, wl, rs, rp, ts, proj='rect', res=300):
+def mkmaps(planet, eigeny, params, npar, ncurves, wl, rs, rp, ts,
+           proj='rect', res=300):
     """
     Calculate flux maps and brightness temperature maps from
     2D map fits.
@@ -96,8 +97,11 @@ def mkmaps(planet, eigeny, params, npar, wl, rs, rp, ts, proj='rect', res=300):
         (npar) * (number of wavelengths)
 
     npar: int
-        Number of parameters in the fit per wavelenght (e.g., a weight
+        Number of parameters in the fit per wavelength (e.g., a weight
         for each eigenmap, a base brightness, and a stellar correction term)
+
+    ncurves: int
+        Number of eigencurves (or eigenmaps) included in the total map.
 
     wl: 1D array
         The wavelengths for each 2D map, in microns.
@@ -123,19 +127,19 @@ def mkmaps(planet, eigeny, params, npar, wl, rs, rp, ts, proj='rect', res=300):
     tmaps: 3D array
         Same as fmaps but for brightness temperature.
     """
-    ncurves = int(len(params) / npar)
-
-    fmaps = np.zeros((ncurves, res, res)) # flux maps
-    tmaps = np.zeros((ncurves, res, res)) # temp maps
+    nwl = len(wl)
+    
+    fmaps = np.zeros((nwl, res, res)) # flux maps
+    tmaps = np.zeros((nwl, res, res)) # temp maps
 
     # Convert wl to m
     wl_m = wl * 1e-6
 
-    for j in range(len(wl)):
+    for j in range(nwl):
         planet.map[1:,:] = 0
 
         fmaps[j] = planet.map.render(theta=180, projection=proj,
-                                    res=res).eval() * params[ncurves]
+                                     res=res).eval() * params[j*npar+ncurves]
 
         for i in range(ncurves):
             planet.map[1:,:] = eigeny[i,1:]
@@ -146,10 +150,84 @@ def mkmaps(planet, eigeny, params, npar, wl, rs, rp, ts, proj='rect', res=300):
         # Convert to brightness temperatures
         # see Rauscher et al., 2018, Eq. 8
         ptemp = (sc.h * sc.c) / (wl_m[j] * sc.k)
+        sfact = 1 + params[j*npar+ncurves+1]
         tmaps[j] = ptemp / np.log(1 + (rp / rs)**2 *
                                   (np.exp(ptemp / ts) - 1) /
-                                  (np.pi * fmaps[j]))
+                                  (np.pi * fmaps[j] * sfact))
 
     return fmaps, tmaps
 
-        
+def emapminmax(planet, eigeny, ncurves):
+    """
+    Calculates the latitudes and longitudes of eigenmap minimum and maximum.
+    Useful for checking for positivity in summed maps. Minimum is calculated
+    with planet.map.minimize. Maximum is planet.map.minimize on a map
+    with inverted sign eigenvalues.
+
+    Arguments
+    ---------
+    planet: starry Planet object
+        Planet object. planet.map will be modified in this function.
+
+    eigeny: 2D array
+        Array of eigenvalues for the eigenmaps. Same form as returned
+        by mkcurves().
+
+    ncurves: int
+        Compute min and max for the first ncurves maps
+
+    Returns
+    -------
+    lat: 1D array
+        Array of latitudes, in degrees, of minimum and maximum of first
+        ncurves maps. Length is 2 * ncurves
+
+    lon: 1D array
+        Array of longitudes, same format as lat.
+
+    intens: 2D array
+        Array of intensities at (lat, lon) for each eigenmap. Shape is
+        (ncurves, nlocations).
+    """
+    lat    = np.zeros(2 * ncurves)
+    lon    = np.zeros(2 * ncurves)
+    intens = np.zeros((ncurves, len(lat)))
+
+    nharm, ny = eigeny.shape
+    
+    lmax = np.int((nharm / 2 + 1)**0.5 - 1)
+
+    # Find min/max locations of each eigenmap
+    for j in range(0, 2 * ncurves, 2):
+        planet.map[1:,:] = 0
+
+        yi = 1
+        for l in range(1, lmax + 1):
+            for m in range(-l, l + 1):
+                planet.map[l, m] = eigeny[j,yi]
+                yi += 1
+
+        lat[j], lon[j], _ = [a.eval() for a in planet.map.minimize()]
+
+        yi = 1
+        for l in range(1, lmax + 1):
+            for m in range(-l, l + 1):
+                planet.map[l, m] = -1 * eigeny[j,yi]
+                yi += 1        
+
+        lat[j+1], lon[j+1], _ = [a.eval() for a in planet.map.minimize()]
+
+    # Compute intensity of each eigenmap at EVERY position
+    for j in range(ncurves):
+        planet.map[1:,:] = 0
+
+        yi = 0
+        for l in range(1, lmax + 1):
+            for m in range(-l, l + 1):
+                planet.map[l, m] = eigeny[j,yi]
+                yi += 1
+
+        for i in range(len(lat)):
+            intens[j,i] = planet.map.intensity(lat=lat[i], lon=lon[i]).eval()
+            
+    return lat, lon, intens

3dmap/lib/model.py
@@ -1,9 +1,21 @@
 import numpy as np
 
-def fit_2d(params, ecurves, t, y00, sflux, ncurves):
+def fit_2d(params, ecurves, t, y00, sflux, ncurves, intens):
     """
     Basic 2D fitting routine for a single wavelength.
     """
+    # Check for negative intensities
+    if type(intens) != type(None):
+        nloc = intens.shape[1]
+        for j in range(nloc):
+            # Weighted eigenmap intensity
+            totint = np.sum(intens[:,j] * params[:ncurves])
+            # Contribution from uniform map
+            totint += params[ncurves] / np.pi
+            if totint < 0:
+                f = np.ones(len(t)) * -1
+                return f
+
     f = np.zeros(len(t))
 
     for i in range(ncurves):
@@ -17,7 +29,7 @@ def fit_2d(params, ecurves, t, y00, sflux, ncurves):
 
     return f
 
-def fit_2d_wl(params, ecurves, t, wl, y00, sflux, ncurves):
+def fit_2d_wl(params, ecurves, t, wl, y00, sflux, ncurves, intens):
     """
     2D fitting driver that calls the 2D fitting routine for each
     wavelength.
@@ -27,9 +39,9 @@ def fit_2d_wl(params, ecurves, t, wl, y00, sflux, ncurves):
     nt   = len(t)
     nw   = len(wl)
     npar = int(len(params) / nw) # params per wavelength
-    for i in range(nw):
+    for i in range(nw):            
         f[i*nt:(i+1)*nt] = fit_2d(params[i*npar:(i+1)*npar], ecurves,
-                                  t, y00, sflux, ncurves)
+                                  t, y00, sflux, ncurves, intens)
 
     return f
                                         

3dmap/run.py
@@ -28,10 +28,6 @@ import plots
 import mkcfg
 import fitclass as fc
 
-# Module imports
-sys.path.append(ratedir)
-import rate
-
 starry.config.quiet = True
 
 def main(cfile):
@@ -75,6 +71,10 @@ def main(cfile):
     print("Running PCA to determine eigencurves.")
     fit.eigeny, fit.evalues, fit.evectors, fit.ecurves, fit.lcs = \
         eigen.mkcurves(system, fit.t, cfg.lmax)
+
+    print("Computing location of minimum and maximum of each eigenmap.")
+    fit.mmlat, fit.mmlon, fit.mmint = eigen.emapminmax(planet, fit.eigeny,
+                                                       cfg.ncurves)
     
     if not os.path.isdir(cfg.outdir):
         os.mkdir(cfg.outdir)
@@ -88,12 +88,18 @@ def main(cfile):
         plots.ecurvepower(fit.evalues, cfg.outdir)
 
     # Set up for MCMC
+    if cfg.posflux:
+        intens = fit.mmint
+    else:
+        intens = None
+        
     indparams = (fit.ecurves, fit.t, fit.wl, fit.pflux_y00,
-                 fit.sflux, cfg.ncurves)
+                 fit.sflux, cfg.ncurves, intens)
 
     npar = cfg.ncurves + 2
 
     params = np.zeros(npar * len(fit.wl))
+    params[cfg.ncurves::npar] = 0.001
     pstep  = np.ones( npar * len(fit.wl)) * 0.01
 
     mc3npz = os.path.join(cfg.outdir, 'mcmc.npz')
@@ -115,12 +121,16 @@ def main(cfile):
     fit.bestfit = mc3out['best_model']
     fit.bestp   = mc3out['bestp']
 
+    print("Best-fit parameters:")
+    print(fit.bestp)
+
     print("Computing total flux and brightness temperature maps.")
     fmaps, tmaps = eigen.mkmaps(planet, fit.eigeny, fit.bestp, npar,
-                                fit.wl, cfg.cfg.getfloat('Star', 'r'),
+                                cfg.ncurves, fit.wl,
+                                cfg.cfg.getfloat('Star', 'r'),
                                 cfg.cfg.getfloat('Planet', 'r'),
                                 cfg.cfg.getfloat('Star', 't'))
-
+    
     if cfg.mkplots:
         plots.pltmaps(tmaps, fit.wl, cfg.outdir, proj='rect')
         plots.bestfit(fit.t, fit.bestfit, fit.flux, fit.ferr, fit.wl,