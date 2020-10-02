d4cf01ac6b2650681712aa555ba62d39b0c79660
Ryan Challener <rchallen@umich.edu>
2020-10-02 16:39:06 -0400

    Implemented visibility function, and handling for partially-visible grid cells

--- 3dmap/lib/taurexclass.py
+++ 3dmap/lib/taurexclass.py
@@ -54,13 +54,17 @@ class EmissionModel3D(taurex.model.EmissionModel):
         self.lonmax = lonmax
 
     def compute_final_flux(self, f_total):
+        # Catch for non-visible (far side) of the planet
+        if ((self.lonmin >= np.pi / 2) or (self.lonmax <= -np.pi / 2)):
+            return np.zeros(len(f_total))
+        
         star_sed = self._star.spectralEmissionDensity
 
         star_radius = self._star.radius
         planet_radius = self._planet.fullRadius
 
-        phimin   = self.lonmin
-        phimax   = self.lonmax
+        phimin   = np.max((self.lonmin, -np.pi / 2))
+        phimax   = np.min((self.lonmax,  np.pi / 2))
         thetamin = self.latmin + np.pi / 2.
         thetamax = self.latmax + np.pi / 2. 
 
@@ -68,8 +72,14 @@ class EmissionModel3D(taurex.model.EmissionModel):
         planet_area = -1 * planet_radius**2  * (phimax - phimin) * \
             (np.cos(thetamax) - np.cos(thetamin))
 
-        star_area = 4 * np.pi * star_radius **2
+        star_area = 2 * np.pi * star_radius **2
 
         grid_flux = (f_total / star_sed) * (planet_area / star_area)
+
+        vis = np.cos(np.mean((self.latmin, self.latmax))) * \
+              np.cos(np.mean((phimin,   phimax  )))
+
+        print(phimin * 180/np.pi, phimax*180/np.pi, vis)
+        grid_flux *= vis
 
         return grid_flux
