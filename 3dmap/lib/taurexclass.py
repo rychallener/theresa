Moved grid cell area calculation from the TauREx emission object to the visibility calculation. Corrected a geometry error. Minor bug fixes.

3dmap/lib/taurexclass.py
@@ -71,10 +71,9 @@ class EmissionModel3D(taurex.model.EmissionModel):
         thetamax = self.latmax + np.pi / 2.
 
         # integral r**2 sin(theta) dtheta dphi
-        planet_area = -1 * planet_radius**2  * (phimax - phimin) * \
-            (np.cos(thetamax) - np.cos(thetamin))
+        planet_area = np.pi * planet_radius ** 2
 
-        star_area = np.pi * star_radius **2
+        star_area = np.pi * star_radius ** 2
 
         cell_flux = (f_total / star_sed) * (planet_area / star_area)
 

3dmap/lib/utils.py
@@ -167,9 +167,9 @@ def readfilters(filterfiles):
 def visibility(t, latgrid, longrid, dlat, dlon, theta0, prot, t0, rp,
                rs, x, y):
     """
-    Calculate the visibility of a grid of cells on a planet
-    at a specific time. Returns a combined visibility based on the
-    observer's line-of-sight and the effect of the star.
+    Calculate the visibility of a grid of cells on a planet at a specific
+    time. Returns a combined visibility based on the observer's
+    line-of-sight, the area of the cells, and the effect of the star.
 
     Arguments
     ---------
@@ -213,6 +213,7 @@ def visibility(t, latgrid, longrid, dlat, dlon, theta0, prot, t0, rp,
     -------
     vis: 2D array
         Visibility of each grid cell. Same shape as latgrid and longrid.
+
     """
     if latgrid.shape != longrid.shape:
         print("Number of latitudes and longitudes do not match.")
@@ -273,22 +274,29 @@ def visibility(t, latgrid, longrid, dlat, dlon, theta0, prot, t0, rp,
                 thetarng = np.array((np.max((thetamin, -np.pi / 2.)),
                                      np.min((thetamax,  np.pi / 2.))))
 
-                # Area correction (some of cell might not be visible)
-                # This is ratio of partial cell to full cell
-                area_corr = np.diff(phirng) * \
-                    np.diff(np.cos(thetarng+np.pi/2)) / \
-                    np.diff((phimax, phimin)) / \
-                    np.diff(np.cos((thetamax+np.pi/2, thetamin+np.pi/2)))
-
-                # Mean visible latitude/longitude
-                thetamean = np.mean(thetarng)
-                phimean   = np.mean(phirng)
 
                 # Visibility based on LoS
-                losvis[i,j] = np.cos(thetamean) * np.cos(phimean) * area_corr
+                # This is the integral of
+                #
+                # A(theta, phi) V(theta, phi) dtheta dphi
+                #
+                # where
+                #
+                # A = r**2 cos(theta)
+                # V = cos(theta) cos(phi)
+                #
+                # Here we've normalized by pi*r**2, since
+                # visibility will be applied to Fp/Fs where planet
+                # size is already taken into account.
+                losvis[i,j] = (np.diff(thetarng/2) + \
+                               np.diff(np.sin(2*thetarng) / 4)) * \
+                    np.diff(np.sin(phirng)) / \
+                    np.pi
 
                 # Grid cell maybe only partially visible
                 if dostar:
+                    thetamean = np.mean(thetarng)
+                    phimean   = np.mean(phirng)
                     # Grid is "within" the star
                     if dgrid(x, y, rp, thetamean, phimean) < rs:
                         starvis[i,j] = 0.0

3dmap/run.py
@@ -309,7 +309,7 @@ def map3d(fit, system):
         taurex.cache.CIACache().set_cia_path(cfg.cfg.get('taurex',
                                                          'ciadir'))
 
-        indparams = [fit, system]
+        indparams = [fit]
         params, pstep, pmin, pmax = model.get_par(fit)
         params = np.array([-1.9010, 1.1419, -1.8112])
         #params = np.array([ 1.9889,  -1.5, -1.5, 30.0,
@@ -324,7 +324,7 @@ def map3d(fit, system):
                          sampler='snooker', savefile=mc3npz,
                          params=params, indparams=indparams,
                          pstep=pstep, pmin=pmin, pmax=pmax,
-                         leastsq='trf', plots=cfg.plots)
+                         leastsq=None, plots=cfg.plots)
 
     fit.specbestp = out['bestp']
 
@@ -335,7 +335,7 @@ def map3d(fit, system):
     fit.fluxgrid, fit.modelwngrid, fit.taugrid = model.specgrid(fit.specbestp,
                                                                 fit,
                                                                 True)
-    fit.specbestmodel = model.sysflux(fit.specbestp, fit, system)
+    fit.specbestmodel = model.sysflux(fit.specbestp, fit)
     fit.specbestmodel = fit.specbestmodel.reshape((nfilt, nt))
         
 