aaf5bf872dccd8d5f087977bd7bc8b84e12210ee
Ryan Challener <rchallen@umich.edu>
2021-03-23 15:25:39 -0400

    Added empty classes to hold 2d and 3d parameters, separately

--- 3dmap/lib/configclass.py
+++ 3dmap/lib/configclass.py
@@ -5,6 +5,8 @@ class Configuration:
     def __init__(self):
         self.planet = Planet()
         self.star   = Star()
+        self.twod   = TwoD()
+        self.threed = ThreeD()
 
 class Planet:
     """
@@ -15,5 +17,17 @@ class Planet:
 class Star:
     """
     A class to hold star parameters.
+    """
+    pass
+
+class TwoD:
+    """
+    A class to hold 2D configuration options.
+    """
+    pass
+
+class ThreeD:
+    """
+    A class to hold 3D configuration options.
     """
     pass
