b67ccb6bdd29ec61bc0a9aadfa15f73e88c3a427
Ryan Challener <rchallen@umich.edu>
2021-09-13 16:49:48 -0400

    Added script to fetch example opacities

---
+++ examples/wasp76b/fetchopac.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+# Fetches ExoTransmit opacities for the example(s)
+
+wget -P ./opac/ https://raw.githubusercontent.com/elizakempton/Exo_Transmit/master/Opac/opacC2H2.dat
+wget -P ./opac/ https://raw.githubusercontent.com/elizakempton/Exo_Transmit/master/Opac/opacH2O.dat
+wget -P ./opac/ https://raw.githubusercontent.com/elizakempton/Exo_Transmit/master/Opac/opacCH4.dat
+wget -P ./opac/ https://raw.githubusercontent.com/elizakempton/Exo_Transmit/master/Opac/opacCO.dat
+wget -P ./opac/ https://raw.githubusercontent.com/elizakempton/Exo_Transmit/master/Opac/opacCO2.dat
+wget -P ./opac/ https://raw.githubusercontent.com/elizakempton/Exo_Transmit/master/Opac/opacNH3.dat
+wget -P ./opac/ https://raw.githubusercontent.com/elizakempton/Exo_Transmit/master/Opac/opacC2H4.dat
+wget -P ./opac/ https://raw.githubusercontent.com/elizakempton/Exo_Transmit/master/Opac/opacC2H2.dat
