31280a15d119765efaae3a5e1c6925b82273c403
Ryan Challener <rchallen@umich.edu>
2021-10-13 13:06:00 -0400

    Update example configuration. Add script to fetch CIAs.

---
+++ examples/wasp76b/fetchcia.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+# Fetches ExoTransmit opacities for the example(s)
+
+wget -P ./cia/ https://hitran.org/data/CIA/H2-H2_2011.cia
+wget -P ./cia/ https://hitran.org/data/CIA/H2-He_2011.cia
