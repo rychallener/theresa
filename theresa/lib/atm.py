Fix bug with sh0. Transfrom 3D SH into real components and normalize for convenience. Add experimental cf scaling factor.

theresa/lib/atm.py
@@ -520,7 +520,11 @@ def pmaps(params, fit):
     
     pmaps = np.zeros(tmaps.shape)
     nmap, ncolumn = pmaps.shape
-    if   mapfunc == 'isobaric':
+    if   mapfunc == 'sh0':
+        # No need to both with spherical harmonics here. Just
+        # assign each pmap to a uniform pressure level.
+        # (Could consider merging with sh1 and generalizing to
+        #  higher order spherical harmonics)
         for i in range(nmap):
             pmaps[i] = 10.**mapparams[i]
     elif mapfunc == 'isobaric2':
@@ -547,7 +551,18 @@ def pmaps(params, fit):
         
         for l in range(lmax+1):
             for m in range(-l, l+1):
-                sh[ish] = ss.sph_harm(m, l, templat, templon)
+                # Real form of the spherical harmonics
+                if m == 0:
+                    sh[ish] = ss.sph_harm(m, l, templon, templat)
+                elif m < 0:
+                    tsh = ss.sph_harm(-m, l, templon, templat)
+                    sh[ish] = 2**0.5 * (-1)**m * np.imag(tsh)
+                elif m > 0:
+                    tsh = ss.sph_harm(m, l, templon, templat)
+                    sh[ish] = 2**0.5 * (-1)**m * np.real(tsh)
+
+                # Normalize to 1 for convenience
+                sh[ish] /= np.max(sh[ish])
                 ish += 1
                 
         for i in range(nmap):

theresa/lib/model.py
@@ -397,7 +397,18 @@ def mcmc_wrapper(params, fit):
     # Integrate cf if asked for
     if fit.cfg.threed.fitcf:
         cfsd = cfsigdiff(fit, tgrid, wn, taugrid, p, pmaps)
-        print("Model Evaluation: {} s".format(time.time() - tic))
+        # Experimental: weight the cf penalty by the relative number
+        #               of grid cells and data points such that the
+        #               effect of the penalty is not dependent on the
+        #               horizontal resolution (oversampling) of the
+        #               model. "factor" is the goal effectiveness of
+        #               the penalty where, e.g., 0.25 means that
+        #               overall the penalty contributes 25% as much to
+        #               the "chisq" as the actual data.
+        factor = 0.03
+        cfsd *= factor * systemflux.shape[0] / cfsd.shape[0]
+        #print(factor * systemflux.shape[0] / cfsd.shape[0])
+        #print("Model Evaluation: {} s".format(time.time() - tic))
         return np.concatenate((systemflux, cfsd))
     
     else:
@@ -629,7 +640,7 @@ def get_par_3d(fit):
     # parameters, as well as sensible initial guesses, parameter
     # boundaries, and step sizes.
     for im, mname in enumerate(fit.cfg.threed.modelnames):   
-        if mname == 'isobaric':
+        if mname == 'sh0':
             npar  = nmaps
             # Guess that higher temps are deeper
             ipar  = np.argsort(np.max(fit.tmaps3d, axis=1))